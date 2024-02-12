8a4fd3589e4aad518a0111b403a0f269ddce23bd
Duc nguyen <ducnguyen150120035555@gmail.com>
2024-02-12 22:38:19 +0700

    finish

--- routes/insert.js
+++ routes/insert.js
@@ -1,10 +1,8 @@
 /** @format */
 
-// /** @format */
+const router = require("express").Router();
+const ctrls = require("../controller/insertData");
+router.post("/", ctrls.insertProduct);
+router.post("/cate", ctrls.insertProductCategory);
 
-// const router = require("express").Router();
-// const ctrls = require("../controller/insertData");
-// router.post("/", ctrls.insertProduct);
-// router.post("/cate", ctrls.insertProductCategory);
-
-// module.exports = router;
+module.exports = router;
